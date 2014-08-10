338063f57eac9f6148ef76f896693c8de2a2f3e7
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-08-10 16:08:50 +0900

    #20 make test/TestSasl.hs

---
+++ test/TestSasl.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedStrings, FlexibleContexts, PackageImports #-}
+
+module TestSasl (
+	ExampleState(..), runTestSasl, fromStdin, fromFile, toStdout) where
+
+import "monads-tf" Control.Monad.State
+import Control.Monad.Trans.Control
+import Data.Pipe
+import System.IO
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC
+
+import Sasl
+
+data ExampleState = ExampleState [(BS.ByteString, BS.ByteString)]
+
+instance SaslState ExampleState where
+	getSaslState (ExampleState s) = s
+	putSaslState s _ = ExampleState s
+
+fromStdin :: MonadIO m => Pipe () BS.ByteString m ()
+fromStdin = fromHandle stdin
+
+fromFile :: (MonadBaseControl IO m, MonadIO m) =>
+	FilePath -> Pipe () BS.ByteString m ()
+fromFile fp = bracket (liftIO $ openFile fp ReadMode) (liftIO . hClose) fromHandle
+
+fromHandle :: MonadIO m => Handle -> Pipe () BS.ByteString m ()
+fromHandle h = liftIO (BS.hGetLine h) >>= (>> fromHandle h) . yield
+
+toStdout :: MonadIO m => Pipe BS.ByteString () m ()
+toStdout = await >>= maybe (return ())
+	((>> toStdout) . liftIO . BSC.putStrLn . BSC.pack . show)
+
+runTestSasl :: Monad m =>
+	Pipe () () (StateT ExampleState m) () -> m ()
+runTestSasl p = do
+	ret <- (`evalStateT` ExampleState []) $ runPipe p
+	case ret of
+		Just _ -> return ()
+		_ -> error "error"
--- test/sasl5.hs
+++ test/sasl5.hs
@@ -1,32 +1,10 @@
-{-# LANGUAGE OverloadedStrings, PackageImports #-}
-
-import "monads-tf" Control.Monad.State
 import Data.Pipe
-
 import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as BSC
 
-import Sasl
+import TestSasl
 import qualified SaslMd5 as MD5
-
-data ExampleState = ExampleState [(BS.ByteString, BS.ByteString)]
-
-instance SaslState ExampleState where
-	getSaslState (ExampleState s) = s
-	putSaslState s _ = ExampleState s
-
-fromStdin :: MonadIO m => Pipe () BS.ByteString m ()
-fromStdin = liftIO BS.getLine >>= yield
-
-toStdout :: MonadIO m => Pipe BS.ByteString () m ()
-toStdout = await >>= maybe (return ())
-	((>> toStdout) . liftIO . BSC.putStrLn . BSC.pack . show)
 
 main :: IO ()
 main = do
 	ps <- BS.getLine
-	ret <- (`evalStateT` ExampleState []) . runPipe $
-		fromStdin =$= MD5.sasl ps =$= toStdout
-	case ret of
-		Just _ -> return ()
-		_ -> putStrLn "error"
+	runTestSasl $ fromFile "md5sv.txt" =$= MD5.sasl ps =$= toStdout
