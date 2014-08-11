52eec430f67e42f27de535ae67afdbd1a63721f4
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-08-11 12:56:37 +0900

    #20 make DIGEST-MD5 using Sasl module

--- test/DigestMd5.hs
+++ test/DigestMd5.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module DigestMd5 (
+	DigestResponse(..), fromDigestResponse,
 	digestMd5,
 ) where
 
+import Control.Applicative
 import Crypto.Hash.MD5
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
@@ -35,3 +37,45 @@ digestMd5 isClient username realm password qop uri nonce nc cnonce = z
 	kd = ha1 +++ ":" +++ nonce +++ ":" +++ nc +++ ":" +++ cnonce +++ ":" +++
 		qop +++ ":" +++ ha2
 	z = hash32 kd
+
+data DigestResponse = DR {
+	drUserName :: BS.ByteString,
+	drRealm :: BS.ByteString,
+	drPassword :: BS.ByteString,
+	drCnonce :: BS.ByteString,
+	drNonce :: BS.ByteString,
+	drNc :: BS.ByteString,
+	drQop :: BS.ByteString,
+	drDigestUri :: BS.ByteString,
+	drCharset :: BS.ByteString }
+	deriving Show
+
+fromDigestResponse :: DigestResponse -> BS.ByteString
+fromDigestResponse = kvsToS . responseToKvs True
+
+kvsToS :: [(BS.ByteString, BS.ByteString)] -> BS.ByteString
+kvsToS [] = ""
+kvsToS [(k, v)] = k `BS.append` "=" `BS.append` v
+kvsToS ((k, v) : kvs) =
+	k `BS.append` "=" `BS.append` v `BS.append` "," `BS.append` kvsToS kvs
+
+responseToKvs :: Bool -> DigestResponse -> [(BS.ByteString, BS.ByteString)]
+responseToKvs isClient rsp = [
+	("username", quote $ drUserName rsp),
+	("realm", quote $ drRealm rsp),
+	("nonce", quote $ drNonce rsp),
+	("cnonce", quote $ drCnonce rsp),
+	("nc", drNc rsp),
+	("qop", drQop rsp),
+	("digest-uri", quote $ drDigestUri rsp),
+	("response", calcMd5 isClient rsp),
+	("charset", drCharset rsp)
+	]
+
+quote :: BS.ByteString -> BS.ByteString
+quote = (`BS.append` "\"") . ("\"" `BS.append`)
+
+calcMd5 :: Bool -> DigestResponse -> BS.ByteString
+calcMd5 isClient = digestMd5 isClient
+	<$> drUserName <*> drRealm <*> drPassword <*> drQop <*> drDigestUri
+	<*> drNonce <*> drNc <*> drCnonce
--- test/digestMd5.hs
+++ test/digestMd5.hs
@@ -6,10 +6,12 @@ import Control.Monad
 import "monads-tf" Control.Monad.State
 import System.IO
 
+import Data.Pipe
+
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
 
-import Sasl
+import Sasl (Sasl(..), SaslState(..), client2server, server2client)
 import TestSasl
 import DigestMd5
 import Papillon
@@ -18,25 +20,106 @@ serverFile, clientFile :: String
 serverFile = "digestMd5sv.txt"
 clientFile = "digestMd5cl.txt"
 
-main :: IO ()
-main = do
+client :: (Monad m, SaslState s) => [StateT s m BS.ByteString]
+client = [mkResponse, return ""]
+
+server :: (Monad m, SaslState s) => [BS.ByteString -> StateT s m ()]
+server = [putReceive, const $ return ()]
+
+mkResponse :: (Monad m, SaslState s) => StateT s m BS.ByteString
+mkResponse = do
+	st <- gets getSaslState
+	let	Just ps = lookup "password" st
+		Just rlm = lookup "realm" st
+		Just n = lookup "nonce" st
+		Just q = lookup "qop" st
+		Just c = lookup "charset" st
+		Just a = lookup "algorithm" st
+		Just un = lookup "username" st
+		uri = "xmpp/localhost"
+		cn = "00DEADBEEF00"
+		nc = "00000001"
+	modify $ putSaslState $ [
+		("username", un),
+		("digest-uri", uri),
+		("nc", nc),
+		("cnonce", cn) ] ++ st
+--		("response", rsp) ] ++ st
+	return . fromDigestResponse $ DR {
+		drUserName = "yoshikuni",
+		drRealm = rlm,
+		drPassword = ps,
+		drCnonce = "00DEADBEEF00",
+		drNonce = n,
+		drNc = "00000001",
+		drQop = q,
+		drDigestUri = "xmpp/localhost",
+		drCharset = c }
+
+exampleInit :: [(BS.ByteString, BS.ByteString)]
+exampleInit = [
+	("password", "password"),
+	("username", "yoshikuni")
+	]
+
+main_ :: IO ()
+main_ = do
 	sv <- openFile serverFile ReadMode
-	cl <- openFile clientFile ReadMode
 	sv1 <- BS.hGetLine sv
 	let s1 = server2client digestMd5Sasl $ return sv1
-	cl1 <- BS.hGetLine cl
-	let s2 = client2server s1 $ return cl1
+	let s2 = client2server s1 mkResponse
 	sv2 <- BS.hGetLine sv
 	let s3 = server2client s2 $ return sv2
-	cl2 <- BS.hGetLine cl
-	let s4 = client2server s3 $ return cl2
+	let s4 = client2server s3 $ return ""
 	case s4 of
-		Result m -> m `runStateT`
-			ExampleState [("password", "password")] >>= print
+		Result m -> m `runStateT` ExampleState exampleInit >>= print
 		_ -> error "bad"
 
+main :: IO ()
+main = do
+	r <- (`runStateT` ExampleState exampleInit) $ do
+		runPipe $ fromFile serverFile
+			=$= pipeCl digestMd5Sasl client server
+			=$= toStdout
+	print r
+	
+
+pipeCl :: Monad m => Sasl s m -> [StateT s m BS.ByteString]
+	-> [BS.ByteString -> StateT s m ()]
+	-> Pipe BS.ByteString BS.ByteString (StateT s m) ()
+pipeCl (Result rslt) _ _ = do
+	r <- lift rslt
+	case r of
+		(True, _) -> yield "success"
+		_ -> yield "failure"
+pipeCl sasl (send : sends) (rcv : rcvs) = await >>= \mbs -> case mbs of
+	Just bs -> do
+		let sasl' = server2client sasl $ rcv bs >> return bs
+		let sasl'' = client2server sasl' send
+		lift $ rcv bs
+		s <- lift send
+		yield s
+		pipeCl sasl'' sends rcvs
+	_ -> return ()
+
+putReceive :: (Monad m, SaslState s) => BS.ByteString -> StateT s m ()
+putReceive bs = do
+	let Just ch = parseAtts bs
+	st <- gets getSaslState
+	let	Just ps = lookup "password" st
+		Just rlm = lookup "realm" ch
+		Just n = lookup "nonce" ch
+		Just q = lookup "qop" ch
+		Just c = lookup "charset" ch
+		Just a = lookup "algorithm" ch
+	modify $ putSaslState $ [
+		("realm", rlm),
+		("nonce", n),
+		("qop", q),
+		("charset", c),
+		("algorithm", a) ] ++ st
+
 digestMd5Sasl :: (Monad m, SaslState s) => Sasl s m
--- digestMd5Sasl :: Monad m => Sasl s m
 digestMd5Sasl =
 	S2C $ \getCh -> C2S $ \getRs -> S2C $ \getRA -> C2S $ \getNl -> Result $ do
 		Just ch <- parseAtts `liftM` getCh
@@ -47,12 +130,6 @@ digestMd5Sasl =
 			Just q = lookup "qop" ch
 			Just c = lookup "charset" ch
 			Just a = lookup "algorithm" ch
-		modify $ putSaslState $ [
-			("realm", rlm),
-			("nonce", n),
-			("qop", q),
-			("charset", c),
-			("algorithm", a) ] ++ st
 		Just rs <- parseAtts `liftM` getRs
 		let	Just un = lookup "username" rs
 			Just uri = lookup "digest-uri" rs
@@ -60,12 +137,6 @@ digestMd5Sasl =
 			Just cn = lookup "cnonce" rs
 			Just rsp = lookup "response" rs
 		st' <- gets getSaslState
-		modify $ putSaslState $ [
-			("username", un),
-			("digest-uri", uri),
-			("nc", nc),
-			("cnonce", cn),
-			("response", rsp) ] ++ st'
 		Just ra <- parseAtts `liftM` getRA
 		"" <- getNl
 		let	Just rspa = lookup "rspauth" ra
