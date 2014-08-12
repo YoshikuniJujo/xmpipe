b0143c1b70f94c6c74043a81730ec00e01b1c9e0
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-08-12 12:07:57 +0900

    #20 make scram example flow

--- sasl/ScramSha1.hs
+++ sasl/ScramSha1.hs
@@ -1,6 +1,12 @@
 {-# LANGUAGE OverloadedStrings, PackageImports #-}
 
 module ScramSha1 (
+	clientFirstMessage,
+	serverFirstMessage,
+	clientFinalMessage,
+	serverFinalMessage,
+
+	exampleFlow,
 	exampleClientProof,
 	exampleServerSignature,
 	) where
@@ -13,6 +19,33 @@ import qualified Data.ByteString.Base64 as B64
 import qualified Crypto.Hash.SHA1 as SHA1
 
 import Hmac
+
+clientFirstMessage :: BS.ByteString -> BS.ByteString -> BS.ByteString
+clientFirstMessage un nnc = "n,," `BS.append` clientFirstMessageBare un nnc
+
+clientFinalMessage :: BS.ByteString -> BS.ByteString -> BS.ByteString -> Int
+	-> BS.ByteString -> BS.ByteString -> BS.ByteString -> BS.ByteString
+clientFinalMessage un ps slt i cb cnnc snnc = BS.concat [
+	clientFinalMessageWithoutProof cb cnnc snnc, ",",
+	"p=", clientProof un ps slt i cb cnnc snnc ]
+
+serverFinalMessage :: BS.ByteString -> BS.ByteString -> BS.ByteString -> Int
+	-> BS.ByteString -> BS.ByteString -> BS.ByteString -> BS.ByteString
+serverFinalMessage un ps slt i cb cnnc snnc = BS.concat
+	["v=", serverSignature un ps slt i cb cnnc snnc]
+
+testFlow :: BS.ByteString -> BS.ByteString -> BS.ByteString -> Int
+	-> BS.ByteString -> BS.ByteString -> BS.ByteString -> BS.ByteString
+testFlow un ps slt i cb cnnc snnc = BS.concat [
+	"C: ", clientFirstMessage un cnnc, "\n",
+	"S: ", serverFirstMessage cnnc snnc slt i, "\n",
+	"C: ", clientFinalMessage un ps slt i cb cnnc snnc, "\n",
+	"S: ", serverFinalMessage un ps slt i cb cnnc snnc, "\n" ]
+
+exampleFlow :: BS.ByteString
+exampleFlow = testFlow
+	"user" "pencil" exampleSalt exampleI
+	"n,," exampleClientNonce exampleServerNonce
 
 int1 :: BS.ByteString
 int1 = "\0\0\0\1"
@@ -38,19 +71,22 @@ storedKey ps salt i = SHA1.hash $ clientKey ps salt i
 clientFirstMessageBare :: BS.ByteString -> BS.ByteString -> BS.ByteString
 clientFirstMessageBare un nnc = BS.concat ["n=", un, ",r=", nnc]
 
-serverFirstMessage :: BS.ByteString -> BS.ByteString -> Int -> BS.ByteString
-serverFirstMessage nnc slt i = BS.concat
-	["r=", nnc, ",s=", B64.encode slt, ",i=", BSC.pack $ show i]
+serverFirstMessage ::
+	BS.ByteString -> BS.ByteString -> BS.ByteString -> Int -> BS.ByteString
+serverFirstMessage cnnc snnc slt i = BS.concat
+	["r=", cnnc, snnc, ",s=", B64.encode slt, ",i=", BSC.pack $ show i]
 
-clientFinalMessageWithoutProof :: BS.ByteString -> BS.ByteString -> BS.ByteString
-clientFinalMessageWithoutProof cb nnc = BS.concat ["c=", B64.encode cb, ",r=", nnc]
+clientFinalMessageWithoutProof ::
+	BS.ByteString -> BS.ByteString -> BS.ByteString -> BS.ByteString
+clientFinalMessageWithoutProof cb cnnc snnc =
+	BS.concat ["c=", B64.encode cb, ",r=", cnnc, snnc]
 
 authMessage :: BS.ByteString -> BS.ByteString -> BS.ByteString -> BS.ByteString
 	-> BS.ByteString -> Int -> BS.ByteString
 authMessage cb un cnnc snnc slt i = BS.concat [
 	clientFirstMessageBare un cnnc, ",",
-	serverFirstMessage (cnnc `BS.append` snnc) slt i, ",",
-	clientFinalMessageWithoutProof cb (cnnc `BS.append` snnc) ]
+	serverFirstMessage cnnc snnc slt i, ",",
+	clientFinalMessageWithoutProof cb cnnc snnc ]
 
 clientSignature :: BS.ByteString -> BS.ByteString -> BS.ByteString -> Int
 	-> BS.ByteString -> BS.ByteString -> BS.ByteString -> BS.ByteString
@@ -59,7 +95,7 @@ clientSignature un ps slt i cb cnnc snnc =
 
 clientProof :: BS.ByteString -> BS.ByteString -> BS.ByteString -> Int
 	-> BS.ByteString -> BS.ByteString -> BS.ByteString -> BS.ByteString
-clientProof un ps slt i cb cnnc snnc =
+clientProof un ps slt i cb cnnc snnc = B64.encode $
 	clientKey ps slt i `xo` clientSignature un ps slt i cb cnnc snnc
 
 serverKey :: BS.ByteString -> BS.ByteString -> Int -> BS.ByteString
@@ -67,7 +103,7 @@ serverKey ps salt i = hmac SHA1.hash 64 (saltedPassword ps salt i) "Server Key"
 
 serverSignature :: BS.ByteString -> BS.ByteString -> BS.ByteString -> Int
 	-> BS.ByteString -> BS.ByteString -> BS.ByteString -> BS.ByteString
-serverSignature un ps slt i cb cnnc snnc = hmac SHA1.hash 64
+serverSignature un ps slt i cb cnnc snnc = B64.encode $ hmac SHA1.hash 64
 	(serverKey ps slt i)
 	(authMessage cb un cnnc snnc slt i)
 
