0d0e50548acbc52db9c3e3f7d2ebab6f16852b71
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-08-14 10:33:40 +0900

    move some value to sasl package

--- Digest.hs
+++ Digest.hs
@@ -8,11 +8,9 @@ import "monads-tf" Control.Monad.Error
 import Data.Pipe
 
 import qualified Data.ByteString as BS
-
 import qualified Network.Sasl as SASL
-import qualified Network.Sasl.DigestMd5.Server as DM5S
 import qualified Network.Sasl.DigestMd5.Client as DM5C
-
+import qualified Network.Sasl.DigestMd5.Server as DM5S
 import qualified Network.Sasl.ScramSha1.Client as SS1C
 import qualified Network.Sasl.ScramSha1.Server as SS1S
 
@@ -28,22 +26,13 @@ saslServers :: (
 	MonadError m, Error (ErrorType m)) => [(
 	BS.ByteString,
 	(Bool, Pipe BS.ByteString (Either SASL.Success BS.ByteString) m ()) )]
-saslServers = [("DIGEST-MD5", digestMd5Sv), ("SCRAM-SHA-1", scramSha1Sv)]
+saslServers = [DM5S.sasl retrieveDM5, SS1S.sasl retrieveSS1]
 
-digestMd5Sv :: (
-	MonadState m, SASL.SaslState (StateType m),
-	MonadError m, Error (ErrorType m)) =>
-	(Bool, Pipe BS.ByteString (Either SASL.Success BS.ByteString) m ())
-digestMd5Sv = SASL.server . DM5S.digestMd5Sv $
-	\"yoshikuni" -> DM5S.mkStored "yoshikuni" "localhost" "password"
+retrieveDM5 :: BS.ByteString -> BS.ByteString
+retrieveDM5 "yoshikuni" = DM5S.mkStored "yoshikuni" "localhost" "password"
+retrieveDM5 _ = error "retrieveDM5: no such user"
 
-scramSha1Sv :: (
-		MonadState m, SASL.SaslState (StateType m),
-		MonadError m, Error (ErrorType m) ) =>
-	(Bool, Pipe BS.ByteString (Either SASL.Success BS.ByteString) m ())
-scramSha1Sv = SASL.server . SS1S.scramSha1Server $
-	\"yoshikuni" -> (slt, stk, svk, i)
-	where
-	slt = "pepper"
-	i = 4492
-	(stk, svk) = SS1S.salt "password" slt i
+retrieveSS1 :: BS.ByteString -> (BS.ByteString, BS.ByteString, BS.ByteString, Int)
+retrieveSS1 "yoshikuni" = (slt, stk, svk, i)
+	where slt = "pepper"; i = 4492; (stk, svk) = SS1S.salt "password" slt i
+retrieveSS1 _ = error "no such user"
