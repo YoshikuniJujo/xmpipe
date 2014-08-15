030d7f746248857ebfd76e646353c0490bd8f1c5
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-08-15 23:19:27 +0900

    #29 bug fix of Digest module after remove clients

--- src/Digest.hs
+++ src/Digest.hs
@@ -1,24 +1,19 @@
 {-# LANGUAGE OverloadedStrings, TypeFamilies, FlexibleContexts, PackageImports #-}
 
 module Digest (
-	SASL.Success(..), SASL.SaslState(..), DM5S.SaslError(..),
-	SASL.SaslErrorType(..),
-	saslClients, saslServers ) where
+	Success(..), SaslState(..), DM5.SaslError(..), SaslErrorType(..),
+	saslServers ) where
 
 import "monads-tf" Control.Monad.State
 import "monads-tf" Control.Monad.Error
 import Data.Pipe
+import Network.Sasl
 
 import qualified Data.ByteString as BS
-import qualified Network.Sasl as SASL
-import qualified Network.Sasl.Plain.Client as PlnC
-import qualified Network.Sasl.Plain.Server as PlnS
-import qualified Network.Sasl.External.Client as ExtC
-import qualified Network.Sasl.External.Server as ExtS
-import qualified Network.Sasl.DigestMd5.Client as DM5C
-import qualified Network.Sasl.DigestMd5.Server as DM5S
-import qualified Network.Sasl.ScramSha1.Client as SS1C
-import qualified Network.Sasl.ScramSha1.Server as SS1S
+import qualified Network.Sasl.Plain.Server as Pln
+import qualified Network.Sasl.External.Server as Ext
+import qualified Network.Sasl.DigestMd5.Server as DM5
+import qualified Network.Sasl.ScramSha1.Server as SS1
 
 import Retrieve
 
@@ -30,21 +25,21 @@ data Retrieve m
 		m (BS.ByteString, BS.ByteString, BS.ByteString, Int))
 
 saslServers :: (
-	MonadState m, SASL.SaslState (StateType m),
-	MonadError m, DM5S.SaslError (ErrorType m)) => [(
+	MonadState m, SaslState (StateType m),
+	MonadError m, DM5.SaslError (ErrorType m)) => [(
 	BS.ByteString,
-	(Bool, Pipe BS.ByteString (Either SASL.Success BS.ByteString) m ()) )]
+	(Bool, Pipe BS.ByteString (Either Success BS.ByteString) m ()) )]
 saslServers = mkSaslServers [
 	RTPlain retrievePln, RTExternal retrieveEx,
 	RTDigestMd5 retrieveDM5, RTScramSha1 retrieveSS1 ]
 
 mkSaslServers :: (
-	MonadState m, SASL.SaslState (StateType m),
-	MonadError m, DM5S.SaslError (ErrorType m)) => [Retrieve m] -> [(
+	MonadState m, SaslState (StateType m),
+	MonadError m, DM5.SaslError (ErrorType m)) => [Retrieve m] -> [(
 	BS.ByteString,
-	(Bool, Pipe BS.ByteString (Either SASL.Success BS.ByteString) m ()) )]
+	(Bool, Pipe BS.ByteString (Either Success BS.ByteString) m ()) )]
 mkSaslServers = map $ \rts -> case rts of
-	RTPlain rt -> PlnS.sasl rt
-	RTExternal rt -> ExtS.sasl rt
-	RTDigestMd5 rt -> DM5S.sasl rt
-	RTScramSha1 rt -> SS1S.sasl rt
+	RTPlain rt -> Pln.sasl rt
+	RTExternal rt -> Ext.sasl rt
+	RTDigestMd5 rt -> DM5.sasl rt
+	RTScramSha1 rt -> SS1.sasl rt
